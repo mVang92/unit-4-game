488bccc5d956f0a0ae32ca8b63cceac8a855391c
mVang <meng.vang92@yahoo.com>
2018-06-02 14:56:00 -0500

    Finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,25 +10,74 @@
 
 // Fun stuff
 $(document).ready(function() {
-var cpuRandomNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;    
-var wins = 0;
-var loses = 0;
-var userNum = 0;
-// Prepend crystal images to HTML
-$("#crystal1").prepend('<img id="crystalOne" src="assets/images/crystal1.png" alt="crystal 1" style="width:100%" />')
-$("#crystal2").prepend('<img id="crystalOne" src="assets/images/crystal2.png" alt="crystal 1" style="width:100%" />')
-$("#crystal3").prepend('<img id="crystalOne" src="assets/images/crystal3.png" alt="crystal 1" style="width:100%" />')
-$("#crystal4").prepend('<img id="crystalOne" src="assets/images/crystal4.png" alt="crystal 1" style="width:100%" />')
+    // Random geneated crystal variables
+    var crystalUno = crystalRandomNumber();
+    var crystalTwo = crystalRandomNumber();
+    var crystalThree = crystalRandomNumber();
+    var crystalFour = crystalRandomNumber();
 
-    // Run the game function
+    // Random cpu number
+    var cpuRandomNum = "";
+
+    // Other global variables   
+    var wins = 0;
+    var loses = 0;
+    var userNum = 0;
+
+    // Place crystal images into HTML
+    $("#crystal1").html('<img src="assets/images/crystal1.png" alt="crystal 1" style="width:100%" />')
+    $("#crystal2").html('<img src="assets/images/crystal2.png" alt="crystal 2" style="width:100%" />')
+    $("#crystal3").html('<img src="assets/images/crystal3.png" alt="crystal 3" style="width:100%" />')
+    $("#crystal4").html('<img src="assets/images/crystal4.png" alt="crystal 4" style="width:100%" />')
+
+    // Override HTML text for numbers and scores
+    $("#winScore").html(wins);
+    $("#loseScore").html(loses);
+    $("#userGuess").html(userNum);
+    $("#randomNum").html(cpuRandomNum);
+
+    // What happens when crystals are clicked
+    // Crystal 1 click function
+    $("#crystal1").on("click", function() {
+        userNum += crystalUno;
+        $("#userGuess").html(userNum);
+        console.log("-clicked crystal 1: " + crystalUno);
+        check();
+    });
+    // Crystal 2 click function
+    $("#crystal2").on("click", function() {
+        userNum += crystalTwo;
+        $("#userGuess").html(userNum);
+        console.log("-clicked crystal 2: " + crystalTwo);
+        check();
+    });
+    // Crystal 3 click function
+    $("#crystal3").on("click", function() {
+        userNum += crystalThree;
+        $("#userGuess").html(userNum);
+        console.log("-clicked crystal 3: " + crystalThree);
+        check();
+    });
+    // Crystal 4 click function
+    $("#crystal4").on("click", function() {
+        userNum += crystalFour;
+        $("#userGuess").html(userNum);
+        console.log("-clicked crystal 4: " + crystalFour);
+        check();
+    });
+
+    // Run the running function
     function running(){
-        // Random geneated variables
-        var crystalUno = crystalRandomNumber();
-        var crystalTwo = crystalRandomNumber();
-        var crystalThree = crystalRandomNumber();
-        var crystalFour = crystalRandomNumber();
-        // Place computer random number into HTML with id="randomNum"
+        // Generate random variables for the crystals again
+        crystalUno = crystalRandomNumber();
+        crystalTwo = crystalRandomNumber();
+        crystalThree = crystalRandomNumber();
+        crystalFour = crystalRandomNumber();
+
+        // Generate random variables for the cpu again
+        cpuRandomNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
         $("#randomNum").html(cpuRandomNum);
+        $("#displayStatus").html('<h2 style="text-align:center">You want to get exactly ' + cpuRandomNum + '.</h2>');
         
         // Test random numbers
         // console.log("Number to reach: " + cpuRandomNum);
@@ -36,44 +85,28 @@ $("#crystal4").prepend('<img id="crystalOne" src="assets/images/crystal4.png" al
         console.log("crystal 2: " + crystalTwo);
         console.log("crystal 3: " + crystalThree);
         console.log("crystal 4: " + crystalFour);
-
-        // What happens when crystals are clicked
-        // Crystal 1 click function
-        $("#crystal1").on("click", function() {
-            userNum += crystalUno;
-            $("#userGuess").html(userNum);
-            check();
-        });
-        // Crystal 2 click function
-        $("#crystal2").on("click", function() {
-            userNum += crystalTwo;
-            $("#userGuess").html(userNum);
-            check();
-        });
-        // Crystal 3 click function
-        $("#crystal3").on("click", function() {
-            userNum += crystalThree;
-            $("#userGuess").html(userNum);
-            check();
-        });
-        // Crystal 4 click function
-        $("#crystal4").on("click", function() {
-            userNum += crystalFour;
-            $("#userGuess").html(userNum);
-            check();
-        });
     }
 
     // Check function to compare user current number to cpu random number
     function check(){
-        if (userNum > cpuRandomNum){
-            console.log("lose");
-            loses++;
-            $("#loseScore").html(loses);
-        } else if (userNum === cpuRandomNum){
-            console.log("win");
+        // Player wins when the number is equal to the cpu random number
+        if (userNum === cpuRandomNum){
             wins++;
+            $("#status").html('<h2 style="text-align:center">You Win!</h2>');
             $("#winScore").html(wins);
+            // Resets user number to 0
+            userNum = 0;
+            running();
+            $("#userGuess").html(userNum);
+        // Player loses when the number is larger than the cpu random number    
+        } else if (userNum > cpuRandomNum){
+            loses++;
+            $("#status").html('<h2 style="text-align:center">Loser!</h2>');
+            $("#loseScore").html(loses);
+            // Resets user number to 0
+            userNum = 0;
+            running();
+            $("#userGuess").html(userNum);
         }
     }
 
@@ -82,18 +115,7 @@ $("#crystal4").prepend('<img id="crystalOne" src="assets/images/crystal4.png" al
         var crystal = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
         return crystal;
     }
-    
-    // Reset the game
-    function start(){
-        var wins = 0;
-        var loses = 0;
-        var userNum = 0;
-        $("#winScore").prepend(wins);
-        $("#loseScore").prepend(loses);
-        $("#userGuess").prepend(userNum);
-        running();
-    }
 
     // Magic begins here
-    start();
+    running();
 });
